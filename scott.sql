083425aec1ee94c8e8a9f77b4ff52643ca861f95
scv2780 <tjdcksgur1@naver.com>
2025-09-24 17:40:27 +0900

    first commit

--- scott.sql
+++ scott.sql
@@ -1,20 +1,141 @@
+SELECT *
+FROM student;
+
+CHAR(2000)     -- 크기가 변하지 않는 것을 담을 떄
+VARCHAR2(4000)
+NUMBER(10,2)
+
+SELECT 1 + '2' -- 오라클은 자동 형변환
+FROM dual
+;
+
+SELECT NAME || '''s ID: ' || ID || ' , WEIGHT is ' || WEIGHT || 'kg' AS "IDAND WEIGHT"
+FROM student;
+
+SELECT *
+FROM emp;
+
+SELECT ename || '(' || JOB || '), ' || ename || '''' || JOB || '''' AS "NAME AND JOB"
+from emp;
+
+SELECT ename || '''s sal is $' || sal AS "Name And Sal"
+FROM emp;
+
+-- 조건절.
 SELECT empno
       ,ename
       ,job
       ,mgr
-FROM emp; -- 보통 대문자로 칼럼은 소문자 이 규칙을 따른다
-
-select * from emp; -- 급할때는 그냥 소문자
-
--- 표현식 사용.
-SELECT empno AS "사원번호" -- 별칭(alias) as 뒤에 ""
-      ,ename "사원이름" -- as 없어도 됌
-      ,'Good Morning !! ' || ename AS "welcome 메세지"
-      ,ename ||'''s 의 급여' || sal AS "급여" -- kim's salary
-FROM emp;
-
--- DISTINCT 중복된 값 제거
-SELECT DISTINCT job, deptno
+      ,hiredate
+      ,sal + comm as "Salary"
+      --,comm
+      ,deptno
+      
 FROM emp
-ORDER BY job DESC; -- ORDER BY는 정렬 DESC 역순
+WHERE empno < '8000'
+AND empno >= '7900'
+AND hiredate LIKE '82%'
+;
 
+SELECT *
+FROM professor
+WHERE pay + nvl (bonus, 0) >= 300
+;
+
+SELECT profno
+      ,LOWER(name) AS "low_name"
+      ,UPPER(id) AS "upp_id"
+      ,INITCAP(position) AS "pos"
+      ,pay
+      ,CONCAT(CONCAT(name, '-'), id) AS "name_id"
+FROM professor
+WHERE LENGTH(name) <> 10
+;
+
+SELECT name
+      ,LENGTH(name) AS "length"
+      ,LENGTHB('홍길동') AS "lengthb"
+      ,SUBSTR(name, 1, 5) AS "substr"
+      ,INSTR(name, 'a') AS "instr"
+      ,pay
+      ,bonus
+      ,LTRIM(LPAD(id, 10, '.'), '.') AS "lpad"
+      ,RTRIM('    Hello, World    ') AS "str"
+      ,REPLACE('Hello', 'H', 'h') AS "replace"
+FROM professor
+WHERE INSTR(UPPER(name), 'A') > 0
+;
+
+SELECT name
+      ,tel
+      ,SUBSTR(tel, 1, instr(tel,')')-1) AS "AREA CODE"
+      ,INSTR(tel,')')-1 AS "val"
+FROM student
+WHERE deptno1 = '201'
+;
+
+SELECT *
+FROM emp
+;
+
+SELECT ename
+      ,REPLACE(ename, SUBSTR(ename,2,2), '--') "REPLACE"
+FROM emp
+;
+
+SELECT *
+FROM student
+;
+
+SELECT name
+      ,jumin
+      ,REPLACE(jumin, SUBSTR(jumin,7,7), '-/-/-/-') "REPLACE"
+FROM student
+;
+
+SELECT name
+      ,tel
+      ,REPLACE(tel, SUBSTR(tel, INSTR(tel, ')')+1, 
+        INSTR(tel, '-')-(INSTR(tel, ')')+1)), '***') "REPLACE"
+FROM student
+WHERE deptno1 = '102'
+;
+
+SELECT name
+      ,tel
+      ,REPLACE(tel, SUBSTR(tel, INSTR(tel, '-')+1, 4), '****') "REPLACE"
+FROM student
+WHERE deptno1 = '101'
+;
+
+SELECT empno
+      ,ename
+      ,job
+      ,ROUND(sal / 12, 2) AS "month"
+      ,TRUNC(sal / 12, 2) AS "trunc"
+      ,MOD(sal, 12) AS "mod"
+      ,CEIL(sal / 12) AS "ceil"
+      ,FLOOR(sal / 12) AS "floor"
+      ,POWER(4, 2) AS "power"
+FROM emp
+;
+
+SELECT MONTHS_BETWEEN('16/01/01', '12/01/01')
+FROM dual
+;
+
+SELECT ADD_MONTHS(SYSDATE, 2)
+      ,NEXT_DAY(SYSDATE+1, '목') AS "next_day"
+      ,LAST_DAY(ADD_MONTHS(SYSDATE, 1)) AS "last"
+FROM dual
+;
+
+SELECT SYSDATE
+      ,TO_CHAR(SYSDATE, 'rrrr-mm-dd hh24:mi:ss') AS "today"
+FROM dual
+WHERE 1=1
+;
+
+SELECT TO_DATE('2025-05-05', 'rrrr-mm-dd hh24') AS "date"
+FROM dual
+;
