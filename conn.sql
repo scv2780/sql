63038789e1adc73fc8f70767bf1cda133c3dab23
scv2780 <tjdcksgur1@naver.com>
2025-10-25 20:17:12 +0900

    1025 vue라우터 혼자 만들기

---
+++ conn.sql
@@ -0,0 +1,13 @@
+select * from player; -- team
+
+select * from team; -- team, stadium
+
+select * from stadium; -- team, schedule
+
+select * from schedule; -- stadium
+
+DESC player;
+
+select rownum
+      ,player_name
+from player;
